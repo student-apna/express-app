7c47e425ac69e79462c87facc0496a61f45407c2
Aftab Alam <aaftabalam18191@gmail.com>
2025-05-20 06:52:56 +0530

    Learn session based Authentication

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
-const app = express();
+import session from 'express-session'; 
 import cookieParser from 'cookie-parser';
+const app = express();
 
 app.use(cookieParser());
 
@@ -9,17 +10,81 @@ app.get('/',(req,res)=>{
     res.send('Hello Express');
 })
 
-app.get('/fetch',(req,res)=>{
-    console.log(req.cookies);
-    res.send('API called');
+// app.get('/fetch',(req,res)=>{
+//     console.log(req.cookies);
+//     res.send('API called');
 
-})
+// })
 
-app.get('/remove-cookie',(req,res)=>{
-    res.clearCookie('name');
-    res.send('Cookie cleared')
-})
+// app.get('/remove-cookie',(req,res)=>{
+//     res.clearCookie('name');
+//     res.send('Cookie')
+// })
 
+
+// session
+
+app.use(session({
+    secret :'your-secret-key',    // change this in production
+    resave:false,                 // Don't save session if unmodified
+    saveUninitialized:true,       // Save new session
+    cookie:{secure:false}          // set to true if using https
+}));
+
+
+// app.get('/visit',(req,res)=>{
+//     if(req.session.page_views){
+//         req.session.page_views++;
+//         res.send(`You visited this page ${req.session.page_views} times`)
+//     }
+//     else{
+//         req.session.page_views=1;
+//         res.send("Welcome to this page for the first time!")
+//     }
+// })
+
+// app.get('/remove-visit',(req,res)=>{
+//     req.session.destroy();
+//     res.send("Session Removed");
+
+// })
+
+
+
+
+// Session Based Authentication
+
+// app.use(express.json())
+// const users = [];
+// app.post('/register',async (req,res)=>{
+//     const {email,password} = req.body;
+//     users.push({
+//         email,password
+//     })
+
+//     res.send('User registered')
+
+// })
+
+// app.post('/login',async (req,res)=>{
+//     const {email,password} = req.body;
+//     const user = users.find(u=> u.email===email);
+//     if(!user || password!==user.password){
+//         return res.send('Not Authorized');
+//     }
+
+//     req.session.user = user;   // create a session
+//     res.send('User Logged In');
+// })
+
+
+// app.get('/dashboard' ,(req,res)=>{
+//     if(!req.session.user){
+//         return res.send('Unauthorized');
+//     }
+
+//     res.send(`Welcome,${req.session.user.email}`);
+// })
 
 
 
